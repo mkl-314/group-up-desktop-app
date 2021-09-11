bfb4c4bfc03be599fbbffcdd1d190ec7dfbe9835
mkl-314 <mingkimlow@gmail.com>
2021-09-11 17:20:14 +1000

    Ignore files in gitignore

--- assignment-problem/src/app/components/ImportStudents.tsx
+++ assignment-problem/src/app/components/ImportStudents.tsx
@@ -153,7 +153,7 @@ const ImportStudents: FC = () => {
       handleWarningMessage("Group size cannot be greater than the number of students.");
     } else if (studentData && +groupSize > studentData.length / 2) {
       handleWarningMessage("Group size will result in only one group!");
-    } else if (+groupSize == 0 || +groupSize == 1) {
+    } else if ((!groupSize && +groupSize == 0) || +groupSize == 1) {
       handleWarningMessage("Group size can't be " + groupSize);
     } else if (groupSize) {
       return true;
